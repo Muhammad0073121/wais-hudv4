58504c61fe5559b1d41640c7868bd0db04290578
Muhammad Abdullah <mabdullah3121@gmail.com>
2024-11-27 11:43:29 +0500

    first commit

---
+++ server.lua
@@ -0,0 +1,20 @@
+
+
+local QBCore = exports['qb-core']:GetCoreObject()
+
+CreateThread(function()
+    while true do
+        local totalPlayer = #GetPlayers()
+        local maxPlayers = GetConvarInt('sv_maxclients', 128)
+        TriggerClientEvent('wais:totalPlayers', -1, totalPlayer, maxPlayers)
+        Wait(Config.RefreshPlayersCountTime)
+    end
+end)
+
+RegisterNetEvent('wais:getTotalPlayers', function()
+    local src = source
+    local totalPlayer = #GetPlayers()
+    local maxPlayers = GetConvarInt('sv_maxclients', 128)
+    TriggerClientEvent('wais:totalPlayers', src, totalPlayer, maxPlayers)
+end)
+
